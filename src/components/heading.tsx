fa6ffa0d87b68e239472249c23be311bfb0ceba6
rbpolim <rbpolim@gmail.com>
2024-07-31 17:21:17 -0300

    feat: new heading component

---
+++ src/components/heading.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  title: string;
+  description?: string;
+};
+
+export function Heading({ title, description }: Props) {
+  return (
+    <>
+      <h2 className="text-sm uppercase">({title})</h2>
+      {!!description && (
+        <p className="mt-4 text-sm text-slate-600/70 text-balance">
+          {description}
+        </p>
+      )}
+    </>
+  );
+}
