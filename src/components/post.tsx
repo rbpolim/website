af35c69b858c4952dac3e9b0b00257a2e5d1f0d2
rbpolim <rbpolim@gmail.com>
2024-07-31 17:21:30 -0300

    feat: new post component

---
+++ src/components/post.tsx
@@ -0,0 +1,32 @@
+type Props = {
+  post: {
+    slug: string;
+    title: string;
+    description: string;
+    date: string;
+  };
+};
+
+export function Post({ post }: Props) {
+  return (
+    <li
+      key={post.slug}
+      className="mt-10 group hover:bg-[#D0F490] transition-colors"
+    >
+      <a
+        href={`/writing/${post.slug}`}
+        className="flex items-start justify-between gap-x-6 text-slate-800"
+      >
+        <div className="flex-1">
+          <h2 className="font-bold text-sm">{post.title}</h2>
+          <p className="text-slate-500/80 text-sm leading-relaxed">
+            {post.description}
+          </p>
+        </div>
+        <div className="ml-auto hidden md:flex">
+          <p className="text-slate-600/40 mt-2 text-xs">{post.date}</p>
+        </div>
+      </a>
+    </li>
+  );
+}
